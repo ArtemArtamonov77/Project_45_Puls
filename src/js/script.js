c1a0362fa331aeabad8e5e70d5ec21f156ec70db
Artem <ArtemWebMastering@gmail.com>
2021-02-21 19:50:26 +0300

    21.02.2021 делаем табы

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,37 @@
-$(document).ready(function(){
+/* Slick слайдер */
+
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"> </button>',
-        nextArrow: '<button type="button" class="slick-prev"><img src="../icons/right.svg"> </button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"> </button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"> </button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true
+                }
+            }   
+        ]   
       });
+  }); */
+
+  /* Tiny-slider */
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    controlsText: [
+        '<img src="icons/left.svg">',
+        '<img src="icons/right.svg">'
+    ]
+  });
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
   });
